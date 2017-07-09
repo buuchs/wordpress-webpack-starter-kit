0439ac7eb5581915d9601aed5a929d3ab65f69aa
Luis Amador <mrluisamador@gmail.com>
2017-07-09 16:27:29 -0400

    Adding in the main JS file for the project

--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ function my_theme_enqueue_styles() {
         get_stylesheet_directory_uri() . '/assets/css/main.css',
         wp_get_theme()->get('Version')
     );
+
+    wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/assets/js/bundle.js', array('jquery'), '',true);
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
