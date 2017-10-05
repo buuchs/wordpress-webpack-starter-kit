8005e242386bd4ff20ad25f060d3aef6e44c08c5
Luis Amador <mrluisamador@gmail.com>
2017-10-04 21:21:18 -0400

    adding WP styleguide to the project

--- functions.php
+++ functions.php
@@ -1,15 +1,21 @@
 <?php
+/**
+ * Functions and definitions
+ *
+ * @file function.php
+ *
+ * @package WordPress
+ */
 function my_theme_enqueue_styles() {
+	$parent_style = 'twentyseventeen'; // This is 'twentyseventeen-style' for the Twenty seventeen theme.
 
-    $parent_style = 'twentyseventeen'; // This is 'twentyseventeen-style' for the Twenty seventeen theme.
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/assets/css/main.css',
-        wp_get_theme()->get('Version')
-    );
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/assets/css/main.css',
+		wp_get_theme()->get( 'Version' )
+	);
 
-    wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/assets/js/bundle.js', array('jquery'), '',true);
+	wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/assets/js/bundle.js', array( 'jquery' ), '',true );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' ); 
-?>
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
